c40bde833f6127642bfd76372a378155eee9502c
nathanielltaylor <ntaylor390@gmail.com>
2015-11-05 17:00:45 -0500

    angular task modules

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -1,17 +1,21 @@
+Tasks = new Mongo.Collection('tasks');
+
 if (Meteor.isClient) {
   // This code only runs on the client
-    angular.module('simple-todos',['angular-meteor']);
+  angular.module('simple-todos',['angular-meteor']);
 
-    angular.module('simple-todos').controller('TodosListCtrl', ['$scope',
-      function ($scope) {
-
-        $scope.tasks = [
-          { text: 'This is task 1' },
-          { text: 'This is task 2' },
-          { text: 'This is task 3' }
-        ];
-
-    }]);
+  angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor',
+    function ($scope, $meteor) {
+      $scope.tasks = $meteor.collection( function() {
+        return Tasks.find({}, { sort: { createdAt: -1 } })
+      });
+      $scope.addTask = function (newTask) {
+        $scope.tasks.push( {
+          text: newTask,
+          createdAt: new Date() }
+        );
+      };
+  }]);
 }
 
 if (Meteor.isServer) {
